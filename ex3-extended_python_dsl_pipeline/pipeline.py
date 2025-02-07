6e5de0d221e73eece5a4f8f9b70bb67f1aa0a4f4
Lee Roland <glroland@hotmail.com>
2025-02-07 15:08:07 -0500

    updated to be functional

--- ex3-extended_python_dsl_pipeline/pipeline.py
+++ ex3-extended_python_dsl_pipeline/pipeline.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import kfp
 import kfp.client
@@ -18,11 +19,11 @@ def register_models():
     pass
 
 @dsl.pipeline(name="ex3 pipeline")
-def train_model_pipeline(git_url: str, db_conn_str: str):
+def train_model_pipeline(git_url: str, env: dict):
 
     ex1_pipeline()
 
-    ex2_pipeline(git_url=git_url)
+    ex2_pipeline(git_url=git_url, env=env)
 
     # Store Assets
     store_assets_task = store_assets()
@@ -41,17 +42,31 @@ token = subprocess.check_output("oc whoami -t", shell=True, text=True).strip()
 
 # Connect to the pipeline server
 print ("Connecting to pipeline server")
-kfp_client = kfp.Client(host="https://ds-pipeline-dspa-baseball.apps.ocp.home.glroland.com/",
+kfp_client = kfp.Client(host="https://ds-pipeline-dspa-mlflow.apps.ocp.home.glroland.com/",
                         existing_token=token,
                         verify_ssl=False)
+
+# Grab local environment variables
+env_to_propagate = dict()
+if "AWS_ACCESS_KEY_ID" in os.environ:
+    env_to_propagate["aws_access_key_id"] = os.environ["AWS_ACCESS_KEY_ID"]
+if "AWS_SECRET_ACCESS_KEY" in os.environ:
+    env_to_propagate["aws_secret_key"] = os.environ["AWS_SECRET_ACCESS_KEY"]
+if "AWS_DEFAULT_REGION" in os.environ:
+    env_to_propagate["aws_default_region"] = os.environ["AWS_DEFAULT_REGION"]
+if "MLFLOW_S3_ENDPOINT_URL" in os.environ:
+    env_to_propagate["mlflow_s3_endpoint_url"] = os.environ["MLFLOW_S3_ENDPOINT_URL"]
+if "MLFLOW_S3_IGNORE_TLS" in os.environ:
+    env_to_propagate["mlflow_s3_ignore_tls"] = os.environ["MLFLOW_S3_IGNORE_TLS"]
 
 # Create a run for the pipeline
 print ("Running Pipeline")
 kfp_client.create_run_from_pipeline_func(
     train_model_pipeline,
-    experiment_name="v3 - experiment",
+    experiment_name="ex3 - experiment",
     arguments={
-        "git_url": "https://github.com/glroland/ml-pipeline-experiments.git"
+        "git_url": "https://github.com/glroland/ml-pipeline-experiments.git",
+        "env": env_to_propagate
     }
 )
 
